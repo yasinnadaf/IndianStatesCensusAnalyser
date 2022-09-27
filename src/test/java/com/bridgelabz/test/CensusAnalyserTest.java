661ce4caec67b514efed970d94bbc0dea35a90da
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-09-27 13:50:05 +0530

    no of record match

--- src/test/java/com/bridgelabz/test/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/test/CensusAnalyserTest.java
@@ -8,8 +8,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class CensusAnalyserTest {
-
-//    static final String PROJECT_PATH = System.getProperty("user.dir");
+    
     private static final String CSV_FILE_PATH = "C:\\CensusAnalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
     @Test
     void givenFileToMatchTheNoOfRecords() throws IOException, CsvException, StateCensusAnalyserException {
