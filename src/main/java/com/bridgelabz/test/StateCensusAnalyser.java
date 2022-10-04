b190eefef90141453df607b719e21922151b426d
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-10-04 16:55:24 +0530

    wrong path

--- src/main/java/com/bridgelabz/test/StateCensusAnalyser.java
+++ src/main/java/com/bridgelabz/test/StateCensusAnalyser.java
@@ -13,12 +13,24 @@ import java.util.List;
 public class StateCensusAnalyser {
   static ArrayList<CSVStateCensus> csvStateCensuses = new ArrayList<>();
 
-  public static int readFile(File file) throws IOException, CsvException {
+  public static int readFile(File file) throws IOException, CsvException, StateCensusAnalyserException {
+    checkInputFile(file);
     FileReader fileReader = new FileReader(file);
     CSVReader csvReader = new CSVReader(fileReader);
     List<String[]> stateCensus = csvReader.readAll();
 
     return stateCensus.size();
   }
+
+   static void checkInputFile(File file) throws StateCensusAnalyserException {
+     if (!file.exists()) {
+       throw new StateCensusAnalyserException("fine not exists");
+     }
+     String expectedFileType = "csv";
+     int index = file.toString().lastIndexOf(".");
+     String actualFileType = file.toString().substring(index + 1);
+     if (!expectedFileType.equals(actualFileType)) throw new StateCensusAnalyserException("file type doesn't match");
+   }
+
 
 }
--- src/test/java/com/bridgelabz/test/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/test/CensusAnalyserTest.java
@@ -10,12 +10,23 @@ import java.io.IOException;
 public class CensusAnalyserTest {
     
     private static final String CSV_FILE_PATH = "C:\\CensusAnalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
+    private static final String CENSUS_INCORRECT_FILE_PATH = "C:\\CensusAnalyser\\src\\main\\resources\\India_StateCensusData.csv" ;
+
     @Test
     void givenFileToMatchTheNoOfRecords() throws IOException, CsvException, StateCensusAnalyserException {
         StateCensusAnalyser.csvStateCensuses.clear();
         int actual =  StateCensusAnalyser.readFile(new File(CSV_FILE_PATH));
         Assertions.assertEquals(30,actual);
     }
+
+    @Test
+    void givenCSVFileIncorrectPath_ShouldThrowException() {
+        File file = new File(CENSUS_INCORRECT_FILE_PATH);
+        Assertions.assertThrows(StateCensusAnalyserException.class,() ->
+                StateCensusAnalyser.readFile(file));
+
+    }
+
 
 
 }
